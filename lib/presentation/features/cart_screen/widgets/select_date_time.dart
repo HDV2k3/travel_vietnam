bebc7f9240067d29c675a96eee699eb1ac0e5bae
Việt Huỳnh <dacviethuynh@gmail.com>
2024-04-27 15:17:18 +0700

    fix widget detail&cart

--- lib/presentation/features/cart_screen/widgets/select_date_time.dart
+++ lib/presentation/features/cart_screen/widgets/select_date_time.dart
@@ -48,6 +48,7 @@ class _SelectDateTimeState extends State<SelectDateTime> {
                 startDate = null;
                 selectedStartDateText = 'Chọn Ngày Bắt Đầu';
                 selectedEndDateText = 'Chọn Ngày Kết Thúc';
+                ///gitworkflow
               });
             },
           );
